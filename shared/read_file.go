eff5c3156ea8c87cec344d6e31195d5520258b10
Kaung Min Htet <kgminhtet.dev@gmail.com>
2025-03-23 16:56:03 +0700

    feat (shared): read file function

---
+++ shared/read_file.go
@@ -0,0 +1,22 @@
+package shared
+
+import (
+	`errors`
+	`fmt`
+	`io/fs`
+	`os`
+)
+
+func ReadFile(filepath string) (*os.File, error) {
+	file, err := os.OpenFile(filepath, os.O_RDONLY, 0444)
+
+	if errors.Is(err, fs.ErrNotExist) {
+		return nil, fmt.Errorf("file does not exist")
+	}
+
+	if err != nil {
+		return nil, fmt.Errorf("unknown error %s", err)
+	}
+
+	return file, nil
+}
---
+++ shared/read_file_test.go
@@ -0,0 +1,33 @@
+package shared
+
+import (
+	`github.com/stretchr/testify/assert`
+	`os`
+	`path`
+	`strings`
+	`testing`
+)
+
+func TestReadFile(t *testing.T) {
+	t.Run("invalid file path", func(t *testing.T) {
+		file, err := ReadFile("./test/data/test.txt")
+		assert.Nil(t, file)
+		assert.NotNil(t, err)
+		assert.EqualError(t, err, "file does not exist")
+	})
+
+	t.Run("valid file path", func(t *testing.T) {
+		// Getting project directory path
+		cd, err := os.Getwd()
+		assert.Nil(t, err, err)
+		splitcd := strings.Split(cd, "/")
+		rootpath := strings.Join(splitcd[:len(splitcd)-1], "/")
+
+		filepath := path.Join(rootpath, "test/data/test.txt")
+		assert.FileExists(t, filepath)
+
+		file, err := ReadFile(filepath)
+		assert.NotNil(t, file)
+		assert.Nil(t, err)
+	})
+}
